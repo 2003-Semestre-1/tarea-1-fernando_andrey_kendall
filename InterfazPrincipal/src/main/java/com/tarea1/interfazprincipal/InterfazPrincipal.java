c38bda02b85ad03e6ed763c23e6b454d17e57ce4
AndreyKdo <4ndreycito@estudiantec.cr>
2023-03-06 09:58:41 -0600

    ADD interfaz

---
+++ InterfazPrincipal/src/main/java/com/tarea1/interfazprincipal/InterfazPrincipal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tarea1.interfazprincipal;
+
+/**
+ *
+ * @author andru
+ */
+public class InterfazPrincipal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
